450415103c7e8101d536594ec4db1a7b6600e554
victor_rxph <v.rxphdev@gmail.com>
2023-11-27 15:42:20 -0300

    Improve page interactvity,create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+(function(){
+
+    let gameBoard = {
+
+        // gameBoardArray: [],
+        gameBoard: Array.from(document.querySelectorAll('.game-square')),
+
+        startGame: function(){
+
+            
+
+        }
+
+    }
+
+    function Player(){
+
+        this.markSquare = function(){
+
+
+
+        }
+        
+        this.win = function(){
+
+
+
+        }
+
+    }
+})();
